c78306b44083f568e5626815f6148107bf82fb05
nicklin <authen@qq.com>
2018-07-09 22:42:31 +0800

    fix doc.sh

--- doc.sh
+++ doc.sh
@@ -1,6 +1,10 @@
 # 创建一个空白分支
-# 复制临时文件目录
-# 清空当前所有暂存 node_modules 不会删除
+# 复制临时文件目录,复制 git 忽略
+# 清空当前所有暂存，并从工作区删除文件，不跟踪的node_modules 不会删除
+# 重命名.gitignore，复制docs内容到根目录
+# 加入git版本库
+# 删除.gitignore，并从工作区删除文件
+# 提交
 git checkout --orphan gh-pages
 cp -R docs docs_temp
 cp -R .gitignore .gitignore2
