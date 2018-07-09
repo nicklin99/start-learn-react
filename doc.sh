4c1d5b5695ed44b70e606bd5492cd877cb3d8062
nicklin <authen@qq.com>
2018-07-09 22:40:33 +0800

    fix doc.sh

--- doc.sh
+++ doc.sh
@@ -1,4 +1,13 @@
+# 创建一个空白分支
+# 复制临时文件目录
+# 清空当前所有暂存 node_modules 不会删除
 git checkout --orphan gh-pages
-git rm -rf ./*
-cp -R docs/* .
+cp -R docs docs_temp
+cp -R .gitignore .gitignore2
+git rm -rf .
+cp -R docs_temp/* .
+mv .gitignore2 .gitignore
+rm .gitignore2
+git add .
+git rm -rf .gitignore
 git commit -m "init docs"
